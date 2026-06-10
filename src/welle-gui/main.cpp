db208d0efbec0af89f6b862aac621c479e34f68b
buggalugz <ross_partington@protonmail.com>
2026-06-10 11:43:42 +0000

    Decouple Qt background thread context from visual layer drops

--- src/welle-gui/main.cpp
+++ src/welle-gui/main.cpp
@@ -63,6 +63,8 @@ int main(int argc, char** argv)
 
     // Create new QT application
     QApplication app(argc, argv);
+    // Decouple event loops from visual layers
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     // Force memory keepalive state
     qputenv("QT_ANDROID_LOW_POWER_COMPLIANT", "0");
     qputenv("QT_CONTAINER_SANDBOX_LOCKDOWN", "1");
