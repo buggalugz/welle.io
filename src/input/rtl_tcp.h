7429919b406112341cdd34286fbb6daee094ae45
buggalugz <ross_partington@protonmail.com>
2026-06-12 03:22:47 +0100

    Change function parameters to uint32_t type
    Updated function signatures for sendVFO, sendRate, and setGainMode to use uint32_t instead of int.

--- src/input/rtl_tcp.h
+++ src/input/rtl_tcp.h
@@ -128,10 +128,11 @@ private:
     const std::array<float, 29>  r82xx_gains{{ 0.0, 0.9, 1.4, 2.7, 3.7, 7.7,
         8.7, 12.5, 14.4, 15.7, 16.6, 19.7, 20.7, 22.9, 25.4, 28.0, 29.7, 32.8,
         33.8, 36.4, 37.2, 38.6, 40.2, 42.1, 43.4, 43.9, 44.5, 48.0, 49.6 }};
+void sendVFO(uint32_t frequency);
+void sendRate(uint32_t theRate);
+void setGainMode(uint32_t gainMode);
 
-void sendVFO(int frequency);
-void sendRate(int theRate);
-void setGainMode(int gainMode);
+
     void sendCommand(uint8_t cmd, int32_t param);
     float getGainValue(uint16_t gainCount);
 };
